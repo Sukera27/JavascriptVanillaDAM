3989ec06b2dac8184d0534e4007061b2c34e2595
Mara2345 <rafa_mancina@hotmail.com>
2025-09-21 11:52:44 +0200

    Cuarto commit

--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -1,9 +1,24 @@
 
-//var
+//var--en desuso en JS
 
 var holaMundo="Hello World";
 console.log(holaMundo);
 
-//let
+holaMundo="Hello World 2";
+console.log(holaMundo);
 
-//const
+// Mejores formas de asignar valores a memoria
+
+//let
+let holaMundo2="Hola Vedruna";
+console.log(holaMundo2);
+
+holaMundo2="Hola otra vez Vedruna";
+console.log(holaMundo2);
+
+//const - no se puede reasignar
+const holaMundo3="Hola Vedruna";
+console.log(holaMundo3);
+
+//holaMundo3="Hola Vedruna costante";
+//console.log(holaMundo3);
