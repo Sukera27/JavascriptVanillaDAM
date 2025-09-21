47cfd758ae0088513d018d3146ffa996c21b11ae
Mara2345 <rafa_mancina@hotmail.com>
2025-09-21 12:41:35 +0200

    Quinto Commit

---
+++ Basic/Ejercicios/Boletin.js
@@ -0,0 +1,10 @@
+//1. Escribe un comentario en una linea
+
+//2. Escribe un comentario en varias lineas
+
+//3. Declara variables con valores asociados a todos los tipos de datos y imprimelos en la consola
+
+//4.Modifica los valores de las variables por otros valores y vuelve a imprimirlos en la consola.
+
+//5. Declara constantes con valores asociados a todos los tipos de datos y imprimelos en la consola.
+
