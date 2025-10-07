a82e85d0556374dc38138ea4c1f2e18bdbe703f7
Mara2345 <rafa_mancina@hotmail.com>
2025-10-07 16:01:13 +0200

    Commit Dieciseis

---
+++ Fetch/EjerciciosFetch/js/EjerciciosFetch.js
@@ -0,0 +1,10 @@
+//##  Ejercicio 1: Mostrar todas las películas en consola
+
+
+//##  Ejercicio 2: Mostrar las películas en el HTML
+
+
+//##  Ejercicio 3: Mostrar detalles de una sola película(Usa el ID)
+
+
+//##  Ejercicio 4: Filtrar películas por género
